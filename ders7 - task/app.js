48e9c46194d64599773246b3b95d620ee54d4306
saida5saida <98975175+saida5saida@users.noreply.github.com>
2022-03-26 16:43:59 +0400

    Add files via upload

---
+++ ders7 - task/app.js
@@ -0,0 +1,60 @@
+// Sual 1 
+
+// function vaxtBitdi() {
+//     console.log("5 saniye bitdi")
+// }
+// setTimeout(vaxtBitdi, 5000)
+
+
+
+// Sual 2 - geri sayimi duzelde bilmedim
+
+// function vaxt() {
+// console.log("Verilen vaxt bitdi")
+// }
+// setTimeout(vaxt, 100000)
+
+
+// Sual 3 
+
+// console.log(Math.round(65.4345))
+
+
+
+// Sual 4 
+
+// function getRandomRange(start, end) {
+//     const number = Math.random()
+//     const difference = end - start;
+//     const multipy = number * difference
+//     return Math.round(multipy + start)
+// }
+// console.log(getRandomRange(0, 10))
+
+// ve ya 
+
+// function randomNumber(start,end) {
+//  return Math.round(Math.random() * (end - start) + start)
+// }
+// console.log(randomNumber(0,10))
+
+
+
+// Sual 5 
+
+// const date = new Date()
+// console.log(date.toDateString())
+
+
+
+// Sual 6 
+
+// const date = new Date()
+// console.log(date.toTimeString().slice(0,8))
+
+
+
+// Sual 7 
+
+// const date = new Date()
+// console.log(date.getDay('March 26, 2023'))
